6817a95ef9cc72e3720198fac51d91b36cea9051
Joe Fearnley <joe.fearnley@gmail.com>
2020-07-07 08:56:23 -0400

    Adding nested serializer relationship for users and items

--- backend/items/serializers.py
+++ backend/items/serializers.py
@@ -2,8 +2,13 @@ from django.contrib.auth.models import User, Group
 from items.models import Item
 from rest_framework import serializers
 
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['title', 'description', 'user', 'due_date', 'created', 'updated']
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    items = serializers.StringRelatedField(many=True, read_only=False)
+    items = ItemSerializer(many=True, read_only=False)
 
     class Meta:
         model = User
@@ -13,8 +18,3 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
-
-class ItemSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Item
-        fields = ['title', 'description', 'user', 'due_date', 'created', 'updated']
