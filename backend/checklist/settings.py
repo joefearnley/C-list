c8849ef1bd7d31492dbd5b735e9d1c83be49b941
Joe Fearnley <joe.fearnley@gmail.com>
2020-12-29 10:28:34 -0500

    Add mysql conf settings

--- backend/checklist/settings.py
+++ backend/checklist/settings.py
@@ -92,8 +92,10 @@ WSGI_APPLICATION = 'checklist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/usr/local/etc/my.cnf',
+        },
     }
 }
 
