7428d2ede42f9da1aca92b2f36245ae2502aeb43
Joe Fearnley <joe.fearnley@gmail.com>
2021-01-17 16:30:21 -0500

    Updating account form components

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -11,7 +11,8 @@ import {
     Form,
     FormGroup,
     FormInput,
-    FormFeedback
+    FormFeedback,
+    Alert
 } from 'shards-react';
 
 class Account extends Component {
@@ -19,73 +20,77 @@ class Account extends Component {
         super(props);
 
         this.state = {
-            username: '',
-            email: '',
+            account: {
+                id: null,
+                email: '',
+            },
+            fields: {
+                email: '',
+            },
             password: '',
-            showPasswordError: false,
+            confirm_password: '',
             showNonFieldError: '',
             nonFieldErrorMessage: '',
-            showAccountUpdatedSuccessMessage: false,
-            accountUpdatedSuccessMessage: '',
+            showEmailError: false,
+            showEmailUpdatedSuccessMessage: false,
+            showPasswordError: false,
             showPasswordUpdatedSuccessMessage: false,
-            passwordUpdatedSuccessMessage: '',
-            showUpdatePasswordError: false,
-            updatePasswordError: '',
-            showPasswordUpdatedSuccessMessage: false,
-            passwordUpdatedSuccessMessage: '',
+            passwordError: '',
         };
+
+        this.dismissEmailUpdateSuccessMessage = this.dismissEmailUpdateSuccessMessage.bind(this);
+        this.dismissPasswordUpdateSuccessMessage = this.dismissPasswordUpdateSuccessMessage.bind(this);
     }
 
     componentDidMount() {
         this.populateForm();
     }
 
+    dismissEmailUpdateSuccessMessage() {
+        this.setState({ showEmailUpdatedSuccessMessage: false });
+    }
+
+    dismissPasswordUpdateSuccessMessage() {
+        this.setState({ showPasswordUpdatedSuccessMessage: false })
+    }
+
     populateForm = e => {
         apiClient.get(`${config.API_URL}/account/`)
             .then(res => {
-
-                console.log(res.data);
-
-                // this.setState({
-                //     email: res.data.email,
-                // });
+                let accountInfo = res.data[0];
+                this.setState({
+                    account: accountInfo,
+                    fields: { 
+                        email: accountInfo.email
+                    }
+                });
             })
             .catch(err => {
-                if(err.response) {
+                if (err.response) {
                     // redirect to login page....
                 }
             });
     }
 
     updateEmail = e => {
-        if (e.target.value !== '') {
-            this.setState({ 
-                username: e.target.value,
-                email: e.target.value,
-                showEmailError: false
-            });
-        }
+        this.setState({
+            fields: {
+                email: e.target.value
+            },
+            showEmailError: false
+        });
     }
 
-    updatePassword = e => {
-        if (e.target.value !== '') {
-            this.setState({
-                password: e.target.value,
-                showPasswordError: false
-            });
-        }
-    }
-
-    submitUpdateAcountForm = e => {
+    submitUpdateAccountForm = e => {
         e.preventDefault();
-        this.setState({ showError: false });
+        this.setState({ showEmailError: false });
 
-        apiClient.post(`${config.API_URL}/token-auth/`, {
-            username: this.state.username,
-            email: this.state.email
+        apiClient.patch(`${config.API_URL}/account/${this.state.account.id}/`, {
+            username: this.state.fields.username,
+            email: this.state.fields.email
         })
         .then(res => {
-            // Show account updated message
+            this.setState({ showEmailUpdatedSuccessMessage: true });
         })
         .catch(err => {
             if (err.response) {
@@ -93,8 +98,48 @@ class Account extends Component {
                     this.setState({ showEmailError: true })
                 }
 
+                if (err.response.data.non_field_errors) {
+                    this.setState(() => ({ 
+                        showNonFieldError: true,
+                        nonFieldErrorMessage: err.response.data.non_field_errors
+                    }));
+                }
+            }
+        });
+    }
+
+    updatePassword = e => {
+        this.setState({
+            password: e.target.value,
+            showPasswordError: false
+        });
+    }
+
+    updateConfirmPassword = e => {
+        this.setState({
+            confirm_password: e.target.value,
+            showPasswordError: false
+        });
+    }
+
+    submitUpdatePasswordForm = e => {
+        e.preventDefault();
+        this.setState({ showPasswordError: false });
+
+        apiClient.post(`${config.API_URL}/account/${this.state.account.id}/change-password/`, {
+            password: this.state.password,
+            confirm_password: this.state.confirm_password
+        })
+        .then(res => {
+            this.setState({ showPasswordUpdatedSuccessMessage: true });
+        })
+        .catch(err => {
+            if (err.response) {
                 if (err.response.data.password) {
-                    this.setState({ showPasswordError: true })
+                    this.setState({ 
+                        passwordError: err.response.data.password,
+                        showPasswordError: true 
+                    })
                 }
 
                 if (err.response.data.non_field_errors) {
@@ -113,12 +158,19 @@ class Account extends Component {
                 <Navigation />
                 <Container>
                     <Row className="justify-content-md-center">
+                        <Col sm="10">
+                            <Alert theme="success" dismissible={ this.dismissEmailUpdateSuccessMessage } open={ this.state.showEmailUpdatedSuccessMessage }>
+                                Your email address has been updated!
+                            </Alert>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
                         <Col sm="8">
                             <h3 className="mb-4">Account Information</h3>
                             <Form>
                                 <FormGroup>
                                     <label htmlFor="email">Email address</label>
-                                    <FormInput invalid={ this.state.showEmailError } id="email" type="email" placeholder={ this.state.email } onChange={this.updateEmail} />
+                                    <FormInput invalid={ this.state.showEmailError } value={ this.state.fields.email || '' } id="email" type="email" onChange={ this.updateEmail } />
                                     <FormFeedback className="text-danger">Please enter valid email address.</FormFeedback>
                                 </FormGroup>
                             </Form>
@@ -127,28 +179,35 @@ class Account extends Component {
                                 { this.state.nonFieldErrorMessage }
                             </p>
 
-                            <Button className="mr-2" theme="primary" onClick={this.submitUpdateAcountForm}>Update</Button>
+                            <Button className="mr-2" theme="primary" onClick={this.submitUpdateAccountForm}>Update</Button>
                         </Col>
                     </Row>
                     <Row className="justify-content-md-center mt-5">
+                        <Col sm="10">
+                            <Alert theme="success" dismissible={ this.dismissPasswordUpdateSuccessMessage } open={ this.state.showPasswordUpdatedSuccessMessage }>
+                                Your password has been updated!
+                            </Alert>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
                         <Col sm="8">
                             <h3 className="mb-4">Change Password</h3>
                             <Form>
                                 <FormGroup>
-                                    <label htmlFor="email">New Password</label>
-                                    <FormInput invalid={ this.state.showError } id="email" type="email" onChange={this.updateEmail} />
+                                    <label htmlFor="password">New Password</label>
+                                    <FormInput invalid={ this.state.showPasswordError } id="password" type="password" onChange={ this.updatePassword } />
                                 </FormGroup>
                                 <FormGroup>
-                                    <label htmlFor="email">Confirm New Password</label>
-                                    <FormInput invalid={ this.state.showPasswordError } id="password" type="password" onChange={this.updatePassword} />
+                                    <label htmlFor="password_confirmation">Confirm New Password</label>
+                                    <FormInput invalid={ this.state.showPasswordError } id="confirm_password" type="password" onChange={ this.updateConfirmPassword } />
                                 </FormGroup>
                             </Form>
 
-                            <p className={this.state.showUpdatePasswordError ? 'show-error' : 'hide-error'}>
-                                { this.state.updatePasswordError }
+                            <p className={this.state.showPasswordError ? 'show-error' : 'hide-error'}>
+                                { this.state.passwordError }
                             </p>
 
-                            <Button className="mr-2" theme="primary" onClick={this.submitUdpatePasswordForm}>Update</Button>
+                            <Button className="mr-2" theme="primary" onClick={ this.submitUpdatePasswordForm }>Update</Button>
                         </Col>
                     </Row>
                 </Container>
