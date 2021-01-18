e42cfe32566e0b66859976ef16134e2a67fab493
Joe Fearnley <joe.fearnley@gmail.com>
2021-01-18 10:46:59 -0500

    Removing react environment variables

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,14 @@
+const urls = {
+    local: 'localhost:8000',
+    test: 'c-lister.herokuapp.com'
+};
+
+const host = window.location.hostname;
+const baseUrl = host === 'localhost' ? urls.local : urls.test;
+const apiUrl = `http://${baseUrl}/api/v1`;
+
 const config = {
-    API_URL: process.env.REACT_APP_API_URL
+    API_URL: apiUrl
 };
 
 export default config;
