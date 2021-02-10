f7319eea4c5e484ee70d2171347e4e197d780078
Joe Fearnley <joe.fearnley@gmail.com>
2021-02-09 19:50:08 -0500

    Allowing joefearnley.com to do requests

--- backend/checklist/settings.py
+++ backend/checklist/settings.py
@@ -73,7 +73,8 @@ MIDDLEWARE = [
 #CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'https://c-lister.herokuapp.com'
+    'https://c-lister.herokuapp.com',
+    'http://joefearnley.com'
 ]
 
 TEST_RUNNER = "redgreenunittest.django.runner.RedGreenDiscoverRunner"
