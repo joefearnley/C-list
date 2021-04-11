c6859e597159c9927ddca28d62ef4b17d088f2c8
Joe Fearnley <joe.fearnley@gmail.com>
2021-04-11 12:34:39 -0400

    Adding complete item and refresh on upcoming UI

--- mobile/src/pages/Upcoming.tsx
+++ mobile/src/pages/Upcoming.tsx
@@ -11,10 +11,13 @@ import {
   IonItemSliding,
   IonItemOptions,
   IonItemOption,
-  IonIcon
+  IonIcon,
+  IonRefresher,
+  IonRefresherContent
 } from '@ionic/react';
+import { RefresherEventDetail } from '@ionic/core';
 import Tabs from '../components/Tabs';
-import { checkmark, trash } from 'ionicons/icons'  
+import { checkmark, trash } from 'ionicons/icons'
 import api from '../api';
 import './Upcoming.css';
 
@@ -25,6 +28,23 @@ const Upcoming: React.FC = () => {
   useEffect(() => {
     loadItems();
   }, []);
+
+  const refreshItems = (event: CustomEvent<RefresherEventDetail>) => {
+
+    api.get(`${api.defaults.baseURL}/items/upcoming`)
+      .then(res => {
+        setItems(res.data);
+      })
+      .catch(err => {
+        if (err.response) {
+          console.log('got an error loading items');
+          console.log(err.response);
+        }
+      })
+      .then(function () {
+        event.detail.complete();
+      });
+  }
 
   const loadItems = () => {
     api.get(`${api.defaults.baseURL}/items/upcoming`)
@@ -54,12 +74,11 @@ const Upcoming: React.FC = () => {
   };
 
   const completeItem = (item: any) => {
-    api.post(`${api.defaults.baseURL}/${item.pk}/items/`, {
+    api.patch(`${api.defaults.baseURL}/items/${item.pk}/`, {
       complete: true
     })
     .then(res => {
-      console.log('item successfully completed.');
-      console.log(res.data);
+      loadItems();
     })
     .catch(err => {
       if (err.response) {
@@ -103,12 +122,10 @@ const Upcoming: React.FC = () => {
 
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Upcoming</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
+        <IonRefresher slot="fixed" onIonRefresh={refreshItems}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle>Upcoming</IonTitle>
